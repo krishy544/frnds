38d9ca024dc0985162bab04fa03047ea414ee6f5
venkata <venkatakishore662@achiever-97.local>
2019-01-09 16:02:39 +0530

    You will check Onecs

---
+++ LOne/LOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LOne
+//
+//  Created by venkata on 1/9/19.
+//  Copyright © 2019 venkata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var array = ["1","2","3","4","5","6"]
+    }
+
+
+}
+
